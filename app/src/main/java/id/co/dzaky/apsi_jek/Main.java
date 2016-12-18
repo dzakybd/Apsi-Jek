da6596fa8cc8ea86b8f428e48f3817043e2e0796
Zaki <dzakybd@gmail.com>
2016-12-18 11:59:25 +0700

    Initial commit

---
+++ app/src/main/java/id/co/dzaky/apsi_jek/Main.java
@@ -0,0 +1,13 @@
+package id.co.dzaky.apsi_jek;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Main extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
